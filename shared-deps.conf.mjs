0aea945ce16cada46867407b9b462ceac320ebc1
Joel Denning <joeldenning@gmail.com>
2021-04-23 16:10:09 -0600

    The rest of the dependencies

--- shared-deps.conf.mjs
+++ shared-deps.conf.mjs
@@ -8,11 +8,36 @@ const config = {
       versions: ["^5.9.0"],
       include: ["lib/system/**"],
     },
+    {
+      name: "react",
+      versions: ["^17"],
+      include: ["umd/**"],
+    },
+    {
+      name: "react-dom",
+      versions: ["^17"],
+      include: ["umd/**"],
+    },
+    {
+      name: "vue",
+      versions: ["^2.6.12"],
+      include: ["dist/vue.js", "dist/vue.min.js"],
+    },
+    {
+      name: "vue-router",
+      versions: ["^3.5.1"],
+      include: ["dist/vue-router.js", "dist/vue-router.min.js"],
+    },
+    {
+      name: "single-spa-angularjs",
+      versions: ["^4.1.0"],
+      include: ["lib/**"],
+    },
   ],
   logLevel: "debug",
   clean: true,
   skipPackagesAtUrl:
-    "https://storage.googleapis.com/polyglot.microforntends.app/npm/",
+    "https://storage.googleapis.com/polyglot.microfrontends.app/npm/",
 };
 
 export default config;
